a395a38fffc0e3a3a8ce9b4ba800daf710dca274
Ryan Vice <ryan@vicesoftware.com>
2021-01-25 17:18:51 -0600

    Added login, logout and profile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,60 @@
 import * as React from 'react';
+import Button from 'react-bootstrap/Button';
 import { ApiStatusIndicator } from './features/apiStatusIndicator';
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <Button className='mb-2' onClick={() => loginWithRedirect()}>Log In</Button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <Button className='mb-2' onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </Button>
+  );
+};
+
+const AuthButtons = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <>
+      {isAuthenticated ? <LogoutButton/> : <LoginButton/> }
+    </>
+  )
+};
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <>
+    {isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <p>{user.email}</p>
+      </div>
+    )} 
+    </>
+  );
+};
 
 function App() {
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Profile/>
+        <AuthButtons/>
         <ApiStatusIndicator/>
       </header>
     </div>
