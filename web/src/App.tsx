bd7a2356c7dfa6d490b1c2289dc5b75ded987765
Ryan Vice <ryan@vicesoftware.com>
2021-01-25 15:48:19 -0600

    Code cleanup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,18 +18,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
         <Badge variant={isHealthy ? 'success' : 'danger'}>{isHealthy ? "API is healthy": "API is not healthy"}</Badge>
       </header>
     </div>
