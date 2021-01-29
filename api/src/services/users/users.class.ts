8e97147d5395b1d5a3392c7854fd6a71eef74180
Ryan Vice <ryan@vicesoftware.com>
2021-01-29 16:33:44 -0600

    Got fix for auth bug where user entity was being fetched by userId instead of auth0Id

--- api/src/services/users/users.class.ts
+++ api/src/services/users/users.class.ts
@@ -1,9 +1,22 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
+import { Id, Params } from '@feathersjs/feathers';
 import { Application } from '../../declarations';
 
 export class Users extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
+  }
+
+  get(id: Id, params?: Params | undefined): Promise<any> {
+    if (params && params.auth0Id) {
+      return this.Model.findAll({
+        where: {
+          auth0Id: id
+        }
+      });
+    }
+    
+    return super.get(id, params);
   }
 }
